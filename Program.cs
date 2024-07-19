7c4bd60b8c98f2bb502c161b768ba213211dc5dc
Matias Leonel Ramirez <matiasleonel.ar>
2024-07-19 16:12:31 -0300

    Update de rama develop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int n1, n2, resultado;
+
+
+Console.WriteLine("Ingrese un Numero:");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese otro numero:");
+n2 = int.Parse(Console.ReadLine());
+
+
+resultado = n1 + n2;
+
+Console.WriteLine("El resultado de la suma es: " + resultado);
+
